925fed87df170fd5995a4db30d5d9541c62e4325
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2021-08-28 23:04:01 +0300

    update creation

--- projects/shadowrun/src/app/5e/components/create-pc-skills/create-pc-skills.component.ts
+++ projects/shadowrun/src/app/5e/components/create-pc-skills/create-pc-skills.component.ts
@@ -1,14 +1,27 @@
 import {ChangeDetectionStrategy, Component, forwardRef, Input, OnInit} from '@angular/core';
-import {ControlValueAccessor, FormArray, FormControl, FormGroup, NG_VALUE_ACCESSOR, Validators} from '@angular/forms';
+import {AbstractControl, ControlValueAccessor, FormArray, FormControl, FormGroup, NG_VALUE_ACCESSOR, Validators} from '@angular/forms';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
 import {BehaviorSubject, combineLatest} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 import {UnsubscribeDirective} from '@shared';
 import {
-  Awakening, Metatype, ATTRIBUTE_ID, AWAKENING_ID, METATYPE_ID, AWAKENINGS, METATYPES, ACTIVE_SKILLS, Skill
+  ACTIVE_SKILLS,
+  ATTRIBUTE_ID,
+  Awakening,
+  AWAKENING_ID,
+  AWAKENINGS,
+  Metatype,
+  METATYPE_ID,
+  METATYPES,
+  Skill,
+  SKILL_ID
 } from '@shadowrun/app/5e';
 
 const SKILL_MIN = 0;
 const SKILL_MAX = 6;
+const SPECIALIZATIONS_MAX = 1;
+const SPECIALIZATIONS_MIN_SKILL_RANK = 3;
 
 @Component({
   /* tslint:disable-next-line */
@@ -27,6 +40,7 @@ const SKILL_MAX = 6;
 export class CreatePcSkillsComponent extends UnsubscribeDirective implements ControlValueAccessor, OnInit {
   @Input() set awakening(value: AWAKENING_ID) { this.awakening$.next(value); }
   @Input() set metatype(value: METATYPE_ID) { this.metatype$.next(value); }
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
   readonly form: FormArray = new FormArray([]);
   readonly skills: Skill[] = ACTIVE_SKILLS;
   private readonly awakening$: BehaviorSubject<AWAKENING_ID> = new BehaviorSubject(null);
@@ -38,7 +52,6 @@ export class CreatePcSkillsComponent extends UnsubscribeDirective implements Con
       this.setSkills(res[0], res[1]);
     })
   );
-  propagateChange = (_: any) => {};
 
   constructor() {
     super();
@@ -46,12 +59,42 @@ export class CreatePcSkillsComponent extends UnsubscribeDirective implements Con
 
   ngOnInit(): void {
     this.subscriptions = this.skills$.subscribe();
-    this.subscriptions = this.form.valueChanges.subscribe(res => this.propagateChange(res));
+    this.subscriptions = this.form.valueChanges
+      .pipe(
+        tap((res: any[]) => {
+          res.forEach((e, i) => {
+            const control: AbstractControl = this.form.controls[i].get('specializations');
+            e.rating < SPECIALIZATIONS_MIN_SKILL_RANK
+              ? control.disable({ emitEvent: false })
+              : control.enable({ emitEvent: false });
+          });
+        })
+      )
+      .subscribe(res => this.propagateChange(res));
   }
-
+  propagateChange = (_: any) => {};
   writeValue(obj: any): void {}
   registerOnChange(fn: any): void { this.propagateChange = fn; }
   registerOnTouched(fn: any): void {}
+
+  getSpecializations(control: AbstractControl): string[] {
+    return control.value ?? [];
+  }
+
+  onAddSpecialization(event: MatChipInputEvent, control: AbstractControl): void {
+    const value: string = (event.value ?? '').trim();
+    const specializations: string[] = control.value ?? [];
+
+    if (value && specializations.length < SPECIALIZATIONS_MAX) {
+      control.setValue([...specializations, value]);
+    }
+
+    event.input.value = '';
+  }
+
+  onRemoveSpecialization(specialization: string, control: AbstractControl): void {
+    control.setValue((control.value ?? []).filter(i => i !== specialization));
+  }
 
   private setSkills(awakeningId: AWAKENING_ID, metatypeId: METATYPE_ID): void {
     this.form.clear();
@@ -60,8 +103,8 @@ export class CreatePcSkillsComponent extends UnsubscribeDirective implements Con
     const metatype: Metatype = METATYPES.find(i => i.id === metatypeId);
 
     ACTIVE_SKILLS
-      .filter(skill => { // Filter out Magic/Resonance skills
-        if (skill.attribute === ATTRIBUTE_ID.MAGIC) {
+      .filter(skill => {
+        if (skill.attribute === ATTRIBUTE_ID.MAGIC || [SKILL_ID.ARCANA, SKILL_ID.ASSENSING, SKILL_ID.ASTRAL_COMBAT].includes(skill.id)) {
           return awakening.attributes[ATTRIBUTE_ID.MAGIC][0] > 0;
         } else if (skill.attribute === ATTRIBUTE_ID.RESONANCE) {
           return awakening.attributes[ATTRIBUTE_ID.RESONANCE][0] > 0;
@@ -75,6 +118,7 @@ export class CreatePcSkillsComponent extends UnsubscribeDirective implements Con
           min: new FormControl(SKILL_MIN),
           max: new FormControl(SKILL_MAX),
           rating: new FormControl(SKILL_MIN, [Validators.required, Validators.min(SKILL_MIN), Validators.max(SKILL_MAX)]),
+          specializations: new FormControl(null)
         }));
       });
   }
