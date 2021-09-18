0b5fffe008779b8781af4bd73154d8c360c60214
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2021-09-18 18:02:59 +0300

    update character creation

--- projects/shadowrun/src/app/profile/profile.component.ts
+++ projects/shadowrun/src/app/profile/profile.component.ts
@@ -1,15 +1,24 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { AuthService } from '@shared';
 
 @Component({
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  onSignOutClick(): void {
+    this.auth.signOut()
+      .pipe(
+        tap(() => this.router.navigate(['/']))
+      )
+      .subscribe();
   }
 
+  onDeleteProfileClick(): void {}
 }
--- projects/shadowrun/src/app/profile/profile.module.ts
+++ projects/shadowrun/src/app/profile/profile.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { SharedModule } from '@shared';
 
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [
-    CommonModule,
+    SharedModule,
     ProfileRoutingModule
   ]
 })
