fa060cd78d20e9ab7bb573441fef019884ebcfae
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2022-09-17 20:50:41 +0300

    update

--- projects/anno-1800/src/app/index/index.component.ts
+++ projects/anno-1800/src/app/index/index.component.ts
@@ -1,6 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {Observable} from 'rxjs';
-import {AuthService} from '@shared';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '@shared';
+import {tap} from 'rxjs/operators';
 
 @Component({
   templateUrl: './index.component.html',
@@ -11,9 +13,17 @@ export class IndexComponent {
   expanded = true;
   readonly logged$: Observable<boolean> = this.auth.logged$;
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private router: Router) {}
 
   onToggleSidenavClick(): void {
     this.expanded = !this.expanded;
+  }
+
+  onLogoutClick(): void {
+    this.auth.signOut()
+      .pipe(
+        tap(() => this.router.navigate(['/auth/sign-in']))
+      )
+      .subscribe();
   }
 }
