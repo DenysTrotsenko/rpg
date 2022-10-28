7f5471d03739bb851093ec4bb87fa21762afe2e7
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2022-10-28 15:34:23 +0300

    update

--- projects/anno-1800/src/app/admin/admin-routing.module.ts
+++ projects/anno-1800/src/app/admin/admin-routing.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from '@ti/app/admin/admin.component';
 import { QualitiesComponent } from '@ti/app/admin/qualities/qualities.component';
+import { AilmentsComponent } from '@ti/app/admin/ailments/ailments.component';
+import { DrugsComponent } from '@ti/app/admin/drugs/drugs.component';
+import { QuirksComponent } from '@ti/app/admin/quirks/quirks.component';
+import { TalentsComponent } from '@ti/app/admin/talents/talents.component';
+import { TraitsComponent } from '@ti/app/admin/traits/traits.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
     children: [
-      {
-        path: 'qualities',
-        component: QualitiesComponent
-      },
+      { path: 'ailments', component: AilmentsComponent },
+      { path: 'drugs', component: DrugsComponent },
+      { path: 'qualities', component: QualitiesComponent },
+      { path: 'quirks', component: QuirksComponent },
+      { path: 'talents', component: TalentsComponent },
+      { path: 'traits', component: TraitsComponent }
     ]
   }
 ];
