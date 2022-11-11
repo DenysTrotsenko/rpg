6f0eceaa8c13e4c9226baba4410c3b985b18a606
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2022-11-11 14:28:04 +0200

    update

--- projects/anno-1800/src/app/index/index.component.ts
+++ projects/anno-1800/src/app/index/index.component.ts
@@ -1,30 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of, switchMap, combineLatest } from 'rxjs';
-import { catchError, filter, tap } from 'rxjs/operators';
-import { AuthService, DialogService, getId, SnackbarService, StorageService } from '@shared';
-import { RISK_FACTORS } from '@flames-of-freedom-1e/risk-factors';
-import { LANGUAGES } from '@flames-of-freedom-1e/languages';
-import { ALCHEMICAL_ARTS, SPELL_TYPES, SPELLS } from '@flames-of-freedom-1e/spells';
-import { THREAT_TRAITS } from '@flames-of-freedom-1e/threat-traits';
-import { THREAT_TYPES } from '@flames-of-freedom-1e/threat-types';
-import { Attribute, Skill, Tier } from '@grim-and-perilous/models/common';
-import { SpellType, ThreatTrait } from '@flames-of-freedom-1e/models';
-import { TIERS } from '@flames-of-freedom-1e/tiers';
-import { WEAPONS } from '@flames-of-freedom-1e/weapons';
-import { SKILLS } from '@flames-of-freedom-1e/skills';
-import { QUALITIES } from '@flames-of-freedom-1e/qualities';
-import { THREATS } from '@flames-of-freedom-1e/threats';
-import { StoragePath } from '@grim-and-perilous/enums';
-import { SIZES } from '@flames-of-freedom-1e/sizes';
-import { NOTCHES } from '@flames-of-freedom-1e/notches';
-import { ATTRIBUTES } from '@flames-of-freedom-1e/attributes';
-import { PROFESSIONS } from '@flames-of-freedom-1e/professions';
-import { TRAITS } from '@flames-of-freedom-1e/traits';
-import { QUIRKS } from '@flames-of-freedom-1e/quirks';
-import { TALENTS } from '@flames-of-freedom-1e/talents';
-import { ARCHETYPES } from '@flames-of-freedom-1e/archetypes';
-import { CULTURES } from '@flames-of-freedom-1e/cultures';
+import { Observable, switchMap } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
+import { AuthService, DialogService } from '@shared';
 
 @Component({
   templateUrl: './index.component.html',
@@ -38,9 +16,7 @@ export class IndexComponent {
   constructor(
     private auth: AuthService,
     private dialog: DialogService,
-    private router: Router,
-    private snackbar: SnackbarService,
-    private storage: StorageService
+    private router: Router
   ) {}
 
   onToggleSidenavClick(): void {
