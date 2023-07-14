c36a1606e9f0e6e09d745ed091e985a8374db4b0
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2023-07-14 12:10:59 +0300

    update

--- projects/std/src/lib/core/auth.guard.ts
+++ projects/std/src/lib/core/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
--- projects/std/src/lib/core/auth.resolver.ts
+++ projects/std/src/lib/core/auth.resolver.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { AuthService } from './auth.service';
@@ -7,7 +7,7 @@ import firebase from 'firebase/compat/app';
 import User = firebase.User;
 
 @Injectable()
-export class AuthResolver implements Resolve<User> {
+export class AuthResolver  {
   constructor(private auth: AuthService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<User> {
