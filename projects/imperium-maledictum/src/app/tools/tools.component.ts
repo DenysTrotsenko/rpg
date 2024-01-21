fe902d210307a737d66a9a231fba8c15b88f2775
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2024-01-21 18:21:18 +0200

    update

---
+++ projects/imperium-maledictum/src/app/tools/tools.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tools',
+  templateUrl: './tools.component.html',
+  styleUrls: ['./tools.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ToolsComponent {
+
+}
