3fc334d242ddf860b965ed401bf7b69403fbf732
DenysTrotsenko <real.denys.trotsenko@gmail.com>
2024-06-06 23:13:50 +0300

    update

--- projects/imperium-maledictum-common/src/public-api.ts
+++ projects/imperium-maledictum-common/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of imperium-maledictum-common
  */
 export * from './lib/data.service';
-export * from './lib/resolvers';
+// export * from './lib/resolvers';
 export * from './lib/imperium-maledictum-common.module';
